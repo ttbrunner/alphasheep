90c88a732d18d8be5f4a72c5871d6fc530d6cd98
Thomas Brunner <35266133+ttbrunner@users.noreply.github.com>
2019-08-16 17:33:57 +0200

    Slight change to rules.

--- controller/game_rules.py
+++ controller/game_rules.py
@@ -23,40 +23,44 @@ class GameRules:
         if card not in cards_in_hand:
             return False
 
-        # Player is leading.
         if len(cards_in_trick) == 0:
-            # Is the card of ruf-suit?
+            # Player is leading.
             if self.game_variant.contract == GameVariant.Contract.rufspiel and self.game_variant.ruf_suit == card.suit:
-                # Can we do "davonlaufen"?
+                # The card is of ruf-suit.
                 if sum(c for c in cards_in_hand if c.suit == card.suit) >= 4:
-                    raise NotImplemented("Davonlaufen not implemented yet!")
-                else:
-                    return False                    # If not, then it's not allowed.
-            else:
-                return True                         # Not ruf-suit: then anything goes.
+                    # We are allowed to perform "davonlaufen".
+                    return True                                         # TODO: Check rules of Davonlaufen again!
+                # Player is not allowed to play the ruf-suit otherwise.
+                return False
 
-        # Player is not leading.
-        first_card = cards_in_trick[0]
-
-        # Player is matching trump
-        if self.game_variant.is_trump(first_card) and self.game_variant.is_trump(card):
-            return True                         # Matching Trump
-
-        # Player is matching suit (non-trump)
-        if first_card.suit == card.suit:
-            # Special case: if it's the ruf-suit and we have the Sau, must play it!
-            if card.suit == self.game_variant.ruf_suit and card.pip != Pip.sau:
-                if Card(suit=self.game_variant.ruf_suit, pip=Pip.sau) in cards_in_hand:
-                    return False
+            # Leading with any card of any other suit is OK.
             return True
 
-        # Player is not matching trump. Could they?
+        # Player is not leading, so they have to match the first card.
+        first_card = cards_in_trick[0]
+
+        if self.game_variant.is_trump(first_card) and self.game_variant.is_trump(card):
+            # Player is matching trump
+            return True
+
+        if first_card.suit == card.suit and not self.game_variant.is_trump(card):
+            # Player is matching suit (non-trump)
+            if self.game_variant.contract == GameVariant.Contract.rufspiel and card.suit == self.game_variant.ruf_suit:
+                # Special case if it's the ruf-suit:
+                rufsau = Card(suit=self.game_variant.ruf_suit, pip=Pip.sau)
+                if card != rufsau and rufsau in cards_in_hand:
+                    # Player has the ruf-sau but did not play it!
+                    return False                                        # TODO: Check rules of Davonlaufen again!
+            # All other cases of matching are OK.
+            return True
+
         if self.game_variant.is_trump(first_card) and any(c for c in cards_in_hand if self.game_variant.is_trump(c)):
+            # Player is not matching trump when they could.
             return False
 
-        # Player is not matching non-trump.
-        if any(c for c in cards_in_hand if c.suit==first_card.suit):
+        if any(c for c in cards_in_hand if c.suit == first_card.suit):
+            # Player is not matching non-trump when they could.
             return False
 
-        # Finally - player is not matching because they can't. This is OK.
+        # Finally - player is not leading and not matching because they can't.
         return True
