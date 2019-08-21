99f0ea25be56485d50d194016a08f334d372152f
Thomas Brunner <35266133+ttbrunner@users.noreply.github.com>
2019-08-21 16:35:55 +0200

    Adapted slurm training script.

--- singularity/cl_run_train.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-#SBATCH -c 4
-#SBATCH --mem 16GB
-#SBATCH --gres=gpu:1
-
-OPENBLAS_NUM_THREADS=1 OMP_NUM_THREADS=1 srun singularity exec --nv singularity/sng-alphasau.img python3 -u train_rl_agent.py
