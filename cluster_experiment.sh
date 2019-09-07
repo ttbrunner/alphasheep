69bf300b92b285551dc92521a743e40b35e2a687
Thomas Brunner <35266133+ttbrunner@users.noreply.github.com>
2019-09-07 12:19:23 +0200

    Adjusted number of procsesses for cluster eval.

--- cluster_experiment.sh
+++ cluster_experiment.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-# Starts a single train job and 4 eval jobs to run on the cluster.
+# Starts a single train job and 2 eval jobs to run on the cluster.
 # Since we are a lazy bunch, we simply fork lots of processes instead of designing a a more efficient implementation :)
 
 sbatch --qos=lowprio _cluster_runner/slurm_train_alphasau.sh "$1"
-sbatch --qos=lowprio _cluster_runner/slurm_eval_alphasau.sh "$1"
-sbatch --qos=lowprio _cluster_runner/slurm_eval_alphasau.sh "$1"
 sbatch --qos=lowprio _cluster_runner/slurm_eval_alphasau.sh "$1"
 sbatch --qos=lowprio _cluster_runner/slurm_eval_alphasau.sh "$1"
